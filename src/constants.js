4f8835944402f8f9f856933d6ca6647e553199f5
Florian CARRA <florianc@sicara.com>
2019-01-06 16:12:34 +0100

    Add v0

---
+++ src/constants.js
@@ -0,0 +1,94 @@
+export const COLOR_TO_SYMBOL = {
+  s: '\u2660',
+  c: '\u2663',
+  h: '\u2665',
+  d: '\u2666'
+};
+
+export const NEXT_PLAYER = {
+  west: 'south',
+  south: 'east',
+  east: 'north',
+  north: 'west'
+};
+
+export const NB_CARDS = 8;
+
+export const PLAYING_CARDS = [
+  '7h',
+  '8h',
+  '9h',
+  '10h',
+  'Jh',
+  'Qh',
+  'Kh',
+  'Ah',
+  '7s',
+  '8s',
+  '9s',
+  '10s',
+  'Js',
+  'Qs',
+  'Ks',
+  'As',
+  '7d',
+  '8d',
+  '9d',
+  '10d',
+  'Jd',
+  'Qd',
+  'Kd',
+  'Ad',
+  '7c',
+  '8c',
+  '9c',
+  '10c',
+  'Jc',
+  'Qc',
+  'Kc',
+  'Ac'
+];
+
+export const TRUMP_POINTS = {
+  J: 20,
+  '9': 14,
+  A: 11,
+  '10': 10,
+  K: 4,
+  Q: 3,
+  '8': 0,
+  '7': 0
+};
+
+export const PLAIN_POINTS = {
+  A: 11,
+  '10': 10,
+  K: 4,
+  Q: 3,
+  J: 2,
+  '9': 0,
+  '8': 0,
+  '7': 0
+};
+
+export const TRUMP_RANKING = {
+  J: 7,
+  '9': 6,
+  A: 5,
+  '10': 4,
+  K: 3,
+  Q: 2,
+  '8': 1,
+  '7': 0
+};
+
+export const PLAIN_RANKING = {
+  A: 7,
+  '10': 6,
+  K: 5,
+  Q: 4,
+  J: 3,
+  '9': 2,
+  '8': 1,
+  '7': 0
+};
--- src/index.js
+++ src/index.js
@@ -1,12 +1,178 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as constants from './constants.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// WHAT ABOUT HIDDEN HANDS?
+// WHAT ABOUT PLAYER TURN..?
+// MOVE ON HOVER
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// KEEP CARDS STATE WHEN REFRESHING PAGE (REDUX..?)
+
+// SORT CARDS IN HAND
+
+// REPLACE CURRENT CARDS WITH {'\u{1F0C2}'}, ... (cf https://en.wikipedia.org/wiki/Playing_cards_in_Unicode)
+
+function settleWinningCard(cards, trumpColor) {}
+
+function shuffleArray(array) {
+  let i = array.length - 1;
+  for (; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
+
+class Card extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicked: false,
+      value: this.props.rawValue.substring(0, this.props.rawValue.length - 1),
+      color: this.props.rawValue.substring(this.props.rawValue.length - 1)
+    };
+  }
+
+  render() {
+    let classNames = 'card ';
+    classNames +=
+      this.state.color === 's' || this.state.color === 'c' ? 'black' : 'red';
+    return (
+      <button
+        className={classNames}
+        onClick={() => alert('Card ' + this.props.rawValue)}
+      >
+        {this.state.value + ' ' + constants.COLOR_TO_SYMBOL[this.state.color]}
+      </button>
+    );
+  }
+}
+
+class Hand extends React.Component {
+  renderCards() {
+    const nbCards = this.props.hasPlayedThisTurn
+      ? constants.NB_CARDS - this.props.round - 1
+      : constants.NB_CARDS - this.props.round;
+    return this.props.rawValues.filter((v, i) => i < nbCards).map(function(v) {
+      return <Card key={v} rawValue={v} />;
+    });
+  }
+
+  hasPlayed() {}
+
+  render() {
+    return (
+      <div className={this.props.player}>
+        <div className="status">
+          {this.props.isCurrentPlayer ? 'Playing...' : 'Waiting...'}
+        </div>
+        <div className="cards-hand">{this.renderCards()}</div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicked: false,
+      turn: 0,
+      dealtCards: constants.PLAYING_CARDS,
+      gameFirstPlayer: 'west',
+      turnFirstPlayer: 'west',
+      currentPlayer: 'west',
+      round: 0
+    };
+  }
+
+  hasPlayedThisTurn(player) {
+    let p = this.state.turnFirstPlayer;
+    while (p !== this.state.currentPlayer) {
+      if (p === player) {
+        return true;
+      }
+      p = constants.NEXT_PLAYER[p];
+    }
+    return false;
+  }
+
+  mockNextPlayer() {
+    this.setState({
+      currentPlayer: constants.NEXT_PLAYER[this.state.currentPlayer]
+    });
+    if (
+      constants.NEXT_PLAYER[this.state.currentPlayer] ===
+      this.state.turnFirstPlayer
+    ) {
+      this.setState({ round: this.state.round + 1 });
+    }
+  }
+
+  componentDidMount() {
+    document.title = 'Belote';
+  }
+
+  render() {
+    return (
+      <>
+        <header> Welcome to Belote my friend </header>
+        <div className="board-game">
+          <Hand
+            player="west"
+            rawValues={this.state.dealtCards.slice(0, 8)}
+            hasPlayedThisTurn={this.hasPlayedThisTurn('west')}
+            isCurrentPlayer={'west' === this.state.currentPlayer}
+            round={this.state.round}
+          />
+          <Hand
+            player="east"
+            rawValues={this.state.dealtCards.slice(8, 16)}
+            hasPlayedThisTurn={this.hasPlayedThisTurn('east')}
+            isCurrentPlayer={'east' === this.state.currentPlayer}
+            round={this.state.round}
+          />
+          <Hand
+            player="north"
+            rawValues={this.state.dealtCards.slice(16, 24)}
+            hasPlayedThisTurn={this.hasPlayedThisTurn('north')}
+            isCurrentPlayer={'north' === this.state.currentPlayer}
+            round={this.state.round}
+          />
+          <Hand
+            player="south"
+            rawValues={this.state.dealtCards.slice(24, 32)}
+            hasPlayedThisTurn={this.hasPlayedThisTurn('south')}
+            isCurrentPlayer={'south' === this.state.currentPlayer}
+            round={this.state.round}
+          />
+        </div>
+        <div className="rules">
+          <p>Heart -> &#x2665;</p>
+          <p>Spade -> &#x2660;</p>
+          <p>Club -> &#x2663;</p>
+          <p>Diamond -> &#x2666;</p>
+          <button
+            onClick={() =>
+              this.setState({
+                dealtCards: shuffleArray(constants.PLAYING_CARDS),
+                round: 0,
+                turnFirstPlayer: 'west',
+                currentPlayer: 'west'
+              })
+            }
+          >
+            DEAL
+          </button>
+          <button onClick={() => this.mockNextPlayer()}>NEXT</button>
+        </div>
+      </>
+    );
+  }
+}
+// ========================================
+
+ReactDOM.render(<Game />, document.getElementById('root'));
