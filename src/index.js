661280dbdb8b9b28afb81b93d1669a8416686225
Florian CARRA <florianc@sicara.com>
2019-03-10 19:13:10 +0100

    Update TODO list

--- src/index.js
+++ src/index.js
@@ -10,12 +10,16 @@ import { Game } from './Game';
  */
 
 /*
-1) TRACK AND FIX BUGS (in the game or technical bugs)
-2) DEFINE 3 DIFFERENT MODES FOR CARDS RENDERING (add 'mode' to props of card, filled by parent (Hand or RoundCards) + update Card.render):
+1) FEATURE: KEEP CARDS STATE WHEN REFRESHING PAGE (REDUX..?)
+2) DISPLAY FEATURES:
+  a) display team that has current contract
+  b) sort cards by point (either plain or trump)
+  c) print winner at the end of game (points > contract)
+  d) always display global score => use a global score that does not reset at new game
+3) DEFINE 3 DIFFERENT MODES FOR CARDS RENDERING (add 'mode' to props of card, filled by parent (Hand or RoundCards) + update Card.render):
   -> hidden = hand of waiting players (all black)
   -> played = round cards (visible but no green/red margin on hovering)
   -> hidden = hand of playing player (current behaviour)
-3) FEATURE: KEEP CARDS STATE WHEN REFRESHING PAGE (REDUX..?)
 4) REPLACE CURRENT CARDS WITH {'\u{1F0C2}'}, ... (cf https://en.wikipedia.org/wiki/Playing_cards_in_Unicode)
 5) FEATURE: AI AGENT: (web services..?)
   a) random
