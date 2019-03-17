313faafca7787aaf8ca3e99212e9e33c1ccf5409
Florian CARRA <florianc@sicara.com>
2019-03-17 19:11:23 +0100

    Update TODO list

--- src/index.js
+++ src/index.js
@@ -10,22 +10,22 @@ import { Game } from './Game';
  */
 
 /*
-1) FEATURE: KEEP CARDS STATE WHEN REFRESHING PAGE (REDUX..?)
-2) DISPLAY FEATURES:
+1) DISPLAY FEATURES:
   a) display team that has current contract
   b) sort cards by point (either plain or trump)
   c) print winner at the end of game (points > contract)
   d) always display global score => use a global score that does not reset at new game
-3) DEFINE 3 DIFFERENT MODES FOR CARDS RENDERING (add 'mode' to props of card, filled by parent (Hand or RoundCards) + update Card.render):
-  -> hidden = hand of waiting players (all black)
-  -> played = round cards (visible but no green/red margin on hovering)
-  -> hidden = hand of playing player (current behaviour)
-4) REPLACE CURRENT CARDS WITH {'\u{1F0C2}'}, ... (cf https://en.wikipedia.org/wiki/Playing_cards_in_Unicode)
-5) FEATURE: AI AGENT: (web services..?)
+2) WHEN NOT PLAYER TURN, SET AN API CALL API FOR NEXT MOVE
+3) FEATURE: AI AGENT: (web services..?)
   a) random
   b) "always play highest possible"
   c) expert
   d) reinforcement learning
+4) DEFINE 3 DIFFERENT MODES FOR CARDS RENDERING (add 'mode' to props of card, filled by parent (Hand or RoundCards) + update Card.render):
+  -> hidden = hand of waiting players (all black)
+  -> played = round cards (visible but no green/red margin on hovering)
+  -> hidden = hand of playing player (current behaviour)
+5) REPLACE CURRENT CARDS WITH {'\u{1F0C2}'}, ... (cf https://en.wikipedia.org/wiki/Playing_cards_in_Unicode)
 6) DYNAMIC EFFECTS
 */
 // ========================================
