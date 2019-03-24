d5246d5cd1779e6893fded440d6c2c5d01fcde21
Florian CARRA <florianc@sicara.com>
2019-03-24 17:15:06 +0100

    Improve game info display

--- src/GameInfo.js
+++ src/GameInfo.js
@@ -3,17 +3,24 @@ import * as constants from './constants.js';
 
 export class GameInfo extends React.Component {
   render() {
+    const trumpSymbol =
+      this.props.trumpColor != null
+        ? constants.COLOR_TO_SYMBOL[this.props.trumpColor]
+        : '-';
+    const contract = this.props.contract != null ? this.props.contract : '-';
     return (
       <div className="game-info">
         <p>
           Current Trump Color:
-          {' ' + constants.COLOR_TO_SYMBOL[this.props.trumpColor]}
+          {' ' + trumpSymbol}
         </p>
         <p>
           Current Contract:
-          {' ' + this.props.contract}
+          {' ' + contract}
         </p>
-        <button onClick={this.props.reset}>RESET</button>
+        <div align="center">
+          <button onClick={this.props.reset}>RESET</button>
+        </div>
       </div>
     );
   }
