6bbdf2ac291415884da8f9af6074958b3c8e16a0
Florian CARRA <florianc@sicara.com>
2019-04-14 17:21:48 +0200

    Update TODO list

--- src/index.js
+++ src/index.js
@@ -10,23 +10,23 @@ import { Game } from './Game';
  */
 
 /*
-1) SET A FLASK API AND PLUG IT TO BELOTE
-  a) set Flask
-  b) define 4 endpoints (random, highest_card, expert, reinforcement)
-  c) define random and highest_card and set remaining ones to random
-  d) use WIP commit to plug API to Belote
-2) AUTOMATE AUCTION
+1) AUTOMATE AUCTION
   a) use a new method placeBidAutomaticallyOrPass
   b) define adapted strategies with flask
   c) plug both of them
+2) EXPERT AGENT
+  a) implement expert play strategy (https://www.draw.io/?state=%7B%22ids%22:%5B%221jWwhTPWHRz6CE27vxq1PgiaGvBKqs8Aw%22%5D,%22action%22:%22open%22,%22userId%22:%22112040441941885221837%22%7D#G1jWwhTPWHRz6CE27vxq1PgiaGvBKqs8Aw)
+  b) define an auction strategy
+  c) implement expert bet strategy in flask
 3) CREATE BELOTE DATASETS
   -> export gameHistory at each game end 
-4) DEFINE 3 DIFFERENT MODES FOR CARDS RENDERING (add 'mode' to props of card, filled by parent (Hand or RoundCards) + update Card.render):
+4) TRAIN REINFORCEMENT AGENT
+5) DEFINE 3 DIFFERENT MODES FOR CARDS RENDERING (add 'mode' to props of card, filled by parent (Hand or RoundCards) + update Card.render):
   -> hidden = hand of waiting players (all black)
   -> played = round cards (visible but no green/red margin on hovering)
   -> hidden = hand of playing player (current behaviour)
-5) REPLACE CURRENT CARDS WITH {'\u{1F0C2}'}, ... (cf https://en.wikipedia.org/wiki/Playing_cards_in_Unicode)
-6) DYNAMIC EFFECTS
+6) REPLACE CURRENT CARDS WITH {'\u{1F0C2}'}, ... (cf https://en.wikipedia.org/wiki/Playing_cards_in_Unicode)
+7) DYNAMIC EFFECTS
 
 REMINDER:
 USE CONSTANTS:
@@ -35,6 +35,8 @@ SOME FEATURE ARE NOT INTEGRATED YET:
 - annonces
 - sans/tout atout
 - (sur)coinche
+BUG:
+During Auction mode, upgrade is broken (displayed bet is not the one taken into account)
 */
 // ========================================
 
